9a16b25000f72de3c08964f2c134b56f808ba6ec
yangxutao <yangxutao361@163.com>
2017-05-26 16:21:35 +0800

    add

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TodayNews' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+pod "SnapKit"
+pod "Kingfisher"
+pod "SVProgressHUD"
+pod "FDFullscreenPopGesture"
+pod "Alamofire"
+pod "SwiftyJSON"
+pod "MJRefresh"
+
+
+
+  # Pods for TodayNews
+
+end
--- TodayNews/AppDelegate.swift
+++ TodayNews/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        let rootTabBarController = YTBaseTabBarController()
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = rootTabBarController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
